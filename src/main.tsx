9ef8cc3e318dedb993860bb373963f07af03129e
Danny Zolp <danny@zolp.dev>
2023-01-11 10:03:36 -0600

    feat: finish initializing project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { MantineProvider } from "@mantine/core";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const firebaseConfig = {
+  apiKey: "AIzaSyDAAZ9I3Xq09Ias2WuVNhm3X376zkwLTzM",
+  authDomain: "tri-m-lessons.firebaseapp.com",
+  projectId: "tri-m-lessons",
+  storageBucket: "tri-m-lessons.appspot.com",
+  messagingSenderId: "703890377241",
+  appId: "1:703890377241:web:a5ac6ff21312fc8120f888",
+  measurementId: "G-230JZ26ZFE"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
